9a2f000f4d5b2228f2deac44d2338caa773060ae
rakdav <rakdav@inbox.ru>
2026-01-16 10:14:06 +0200

    Добавьте файлы проекта.

---
+++ Lab1/Program.cs
@@ -0,0 +1,55 @@
+﻿abstract class Figure
+{
+    public abstract double getArea();
+    public abstract double getPerimetr();
+}
+class Rectangle : Figure
+{
+    private double width;
+    private double height;
+    public double Width
+    {
+        get { return width; }
+        set { if(value>0) width = value; }
+    }
+    public double Height
+    {
+        get { return height; }
+        set { if (value > 0) height = value; }
+    }
+    public Rectangle(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+    public override double getArea()=>width * height;
+    public override double getPerimetr()=>2 * (height + width);
+}
+class Circle : Figure
+{
+    private double radius;
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+    public double Radius
+    {
+        get { return radius; }
+        set { if (value > 0) radius = value; }
+    }
+    public override double getArea() => Math.PI * radius * radius;
+    public override double getPerimetr()=>2*Math.PI * radius;
+}
+
+class Cylinder : Figure
+{
+    public Circle? Circle { get; set; }
+    private double height;
+    public double Height
+    {
+        get => height;
+        set { if(value > 0) height = value; }
+    }
+    public override double getArea()=>2*Math.PI* (height+Circle!.Radius);
+    public override double getPerimetr() => 2 * (2 * Circle!.Radius + height);
+}
