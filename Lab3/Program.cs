b5d72264833443f24f8aafc2419eba61aa945dea
rakdav <rakdav@inbox.ru>
2026-01-30 10:35:43 +0200

    Lab4-1

---
+++ Lab3/Program.cs
@@ -0,0 +1,127 @@
+﻿//Person person=new Person();
+//person.Move();
+//Car car=new Car();
+//car.Move();
+//car.Compute();
+//interface IMovable
+//{
+//    void Move();
+//    void Eat();
+//}
+//interface ICompute
+//{
+//    void Compute();
+//}
+//class Person : IMovable
+//{
+//    public void Eat()
+//    {
+//        Console.WriteLine("Человек питается");
+//    }
+//    public void Move()=>Console.WriteLine("Человек идет");
+//}
+//class Car : IMovable, ICompute
+//{
+//    public void Compute() => Console.WriteLine("Вычисляет расход топлива");
+
+//    public void Eat()
+//    {
+//        Console.WriteLine("Машина заправлется");
+//    }
+//    public void Move() => Console.WriteLine("Машина едет");
+//}
+
+Console.Write("Введите ширину прямоугольника:");
+double height = double.Parse(Console.ReadLine()!);
+Console.Write("Введите длину прямоугольника:");
+double width = double.Parse(Console.ReadLine()!);
+Rectangle rectangle = new Rectangle(width, height);
+Console.WriteLine(rectangle);
+
+Console.Write("Введите радиус окружности:");
+double radius = double.Parse(Console.ReadLine()!);
+Circle circle = new Circle(radius);
+Console.WriteLine(circle);
+
+Console.Write("Введите высоту цилиндра:");
+double h = double.Parse(Console.ReadLine()!);
+Cylinder cylinder = new Cylinder(circle, h);
+Console.WriteLine(cylinder);
+interface Figure
+{
+    double getArea();
+    double getPerimetr();
+}
+
+class Rectangle : Figure
+{
+    private double width;
+    private double height;
+    public double Width
+    {
+        get { return width; }
+        set { if (value > 0) width = value; }
+    }
+    public double Height
+    {
+        get { return height; }
+        set { if (value > 0) height = value; }
+    }
+    public Rectangle(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+    public double getArea() => width * height;
+    public double getPerimetr() => 2 * (height + width);
+
+    public override string? ToString()
+    {
+        return $"Прямоугольник длиной {height} и шириной {width} имеет площадь " +
+            $"{getArea():F2} и периметр {getPerimetr():F2}";
+    }
+}
+class Circle : Figure
+{
+    private double radius;
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+    public double Radius
+    {
+        get { return radius; }
+        set { if (value > 0) radius = value; }
+    }
+    public double getArea() => Math.PI * radius * radius;
+    public double getPerimetr() => 2 * Math.PI * radius;
+    public override string? ToString()
+    {
+        return $"Окружность радиусом {radius} имеет площадь " +
+            $"{getArea():F2} и периметр {getPerimetr():F2}";
+    }
+}
+
+class Cylinder : Figure
+{
+    public Circle? Circle { get; set; }
+    private double height;
+
+    public Cylinder(Circle? circle, double height)
+    {
+        Circle = circle;
+        this.height = height;
+    }
+
+    public double Height
+    {
+        get => height;
+        set { if (value > 0) height = value; }
+    }
+    public double getArea() => 2 * Math.PI * (height + Circle!.Radius);
+    public double getPerimetr() => 2 * (2 * Circle!.Radius + height);
+    public override string? ToString()
+    {
+        return $"Цилиндр радиусом основания {Circle!.Radius} и высотой {height} имеет площадь {getArea():F2} и периметр сечения {getPerimetr():F2}";
+    }
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,35 @@
+﻿Person tom=new Person("Tom",23,new Company("Samsung"));
+Person bob = (Person)tom.Clone();
+Console.WriteLine(tom);
+Console.WriteLine(bob);
+bob.Name = "Vasya";
+bob.Company.Name = "Sony";
+Console.WriteLine(tom);
+Console.WriteLine(bob);
+class Person:ICloneable
+{
+    public string? Name { get; set; }
+    public int Age {  get; set; }
+    public Company Company { get; set; }
+    public Person(string? name, int age,Company company)
+    {
+        Name = name;
+        Age = age;
+        Company = company;
+    }
+    public override string? ToString()
+    {
+        return $"Name:{Name} Age:{Age} Company:{Company.Name}";
+    }
+
+    public object Clone()
+    {
+        return new Person(Name, Age,new Company(Company.Name));
+       // return MemberwiseClone();
+    }
+}
+class Company
+{
+    public string? Name { get; set; }
+    public Company(string? name)=>Name = name;
+}
