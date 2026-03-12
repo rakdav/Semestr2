13ab9117f2bbd722949c25ced192719b0fe16573
rakdav <rakdav@inbox.ru>
2026-03-12 14:43:03 +0200

    os1

--- OS1/Program.cs
+++ OS1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Reflection;
+using System.Threading;
 
 //Process current=Process.GetCurrentProcess();
 //Console.WriteLine($"Id:{current.Id}");
@@ -62,22 +63,163 @@ using System.Reflection;
 //t1.Start();
 //t2.Start();
 //t3.Start();
-Thread thread = new Thread(Print);
-thread.Start();
-for (int i = 0; i < 5; i++)
-{
-    Console.WriteLine($"Главный поток:{i}");
-    Thread.Sleep(500);
-}
+//1
+//Thread thread = new Thread(Print);
+//thread.Start();
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine($"Главный поток:{i}");
+//    Thread.Sleep(500);
+//}
+
+//void Print()
+//{
+//	for (int i = 0; i < 5; i++)
+//	{
+//        Console.WriteLine($"Второй поток:{i}");
+//		Thread.Sleep(1000);
+//	}
+//}
+
+//2-Потоки с параметрами и ParameterizedThreadStart
+
+//Thread thread1 = new Thread(new ParameterizedThreadStart(Print));
+//thread1.Start("Hello");
+//Thread thread2=new Thread(PrintSquare);
+//thread2.Start(6);
+
+//void Print(object? message)=>Console.WriteLine(message);
+//void PrintSquare(object? obj)
+//{
+//    if(obj is int n)
+//    {
+//        Console.WriteLine($"{n}*{n}={n*n}");
+//    }
+//}
+//void PrintPerson(object? obj)
+//{
+//    if(obj is Person person)
+//    {
+//        Console.WriteLine($"Name={person.Name}");
+//        Console.WriteLine($"Age={person.Age}");
+//    }
+//}
+//record class Person(string Name,int Age);
+
+//int x = 0;
+//object locker=new();
+//Lock locker=new Lock();
+//AutoResetEvent waitHandler=new AutoResetEvent(true);
+//Mutex mutex = new();
+//for (int i = 1; i < 6; i++)
+//{
+//	Thread myThread = new Thread(Print);
+//	myThread.Name = $"Поток {i}";
+//	myThread.Start();
+//}
+
 
 void Print()
 {
-	for (int i = 0; i < 5; i++)
-	{
-        Console.WriteLine($"Второй поток:{i}");
-		Thread.Sleep(1000);
-	}
+    //1-lock
+    //lock (locker)
+    //{
+    //	//x = 1;
+    //	for (int i = 1; i < 6; i++)
+    //	{
+    //		Console.WriteLine($"{Thread.CurrentThread.Name}:{x}");
+    //		x++;
+    //		Thread.Sleep(100);
+    //	}
+    //}
+    //2-монитор
+    //bool acquiredLock=false;
+    //try
+    //{
+    //	Monitor.Enter(locker,ref acquiredLock);
+    ////	x = 1;
+    //	for (int i = 1; i < 6; i++)
+    //	{
+    //		Console.WriteLine($"{Thread.CurrentThread.Name}:{x}");
+    //		x++;
+    //		Thread.Sleep(100);
+    //	}
+    //}
+    //   finally
+    //{
+    //	if(acquiredLock) Monitor.Exit(locker);
+    //}
+    //3
+    //lock (locker)
+    //{
+    //    //x = 1;
+    //    locker.Enter();
+    //    try
+    //    {
+    //        for (int i = 1; i < 6; i++)
+    //        {
+    //            Console.WriteLine($"{Thread.CurrentThread.Name}:{x}");
+    //            x++;
+    //            Thread.Sleep(100);
+    //        }
+    //    }
+    //    finally {  locker.Exit(); }
+    //}
+    //4-AutoResetEvent
+    //Reset(): задает несигнальное состояние объекта, блокируя потоки.
+    //Set(): задает сигнальное состояние объекта, позволяя одному или нескольким ожидающим потокам продолжить работу.
+    //WaitOne(): задает несигнальное состояние и блокирует текущий поток, пока текущий объект AutoResetEvent не получит сигнал.
+
+    //waitHandler.WaitOne();
+    ////x = 1;
+    //for (int i = 1; i < 6; i++)
+    //{
+    //    Console.WriteLine($"{Thread.CurrentThread.Name}:{x}");
+    //    x++;
+    //    Thread.Sleep(100);
+    //}
+    //waitHandler.Set();
+    //mutex.WaitOne();
+    ////x = 1;
+    //for (int i = 1; i < 6; i++)
+    //{
+    //    Console.WriteLine($"{Thread.CurrentThread.Name}:{x}");
+    //    x++;
+    //    Thread.Sleep(100);
+    //}
+    //mutex.ReleaseMutex();
+
+    //Семафор
+
 }
 
-
-
+for (int i = 1; i < 6; i++)
+{
+    Reader reader = new Reader(i);
+}
+public class Reader
+{
+    static Semaphore sem = new Semaphore(3, 3);
+    Thread myThread;
+    int count = 3;
+    public Reader(int i)
+    {
+        myThread = new Thread(Read);
+        myThread.Name = $"Читатель {i}";
+        myThread.Start();
+    }
+    public void Read()
+    {
+        while (count > 0)
+        {
+            sem.WaitOne();
+            Console.WriteLine($"{Thread.CurrentThread.Name} входит в библиотеку");
+            Console.WriteLine($"{Thread.CurrentThread.Name} читает");
+            Thread.Sleep(1000);
+            Console.WriteLine($"{Thread.CurrentThread.Name} покидает библиотеку");
+            sem.Release();
+            count--;
+            Thread.Sleep(1000);
+        }
+    }
+}
