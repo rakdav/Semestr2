849e580cdec2e8e53c49e666e10e70a97be32750
rakdav <rakdav@inbox.ru>
2026-03-14 12:01:12 +0200

    lab9-2

--- Lab9/Program.cs
+++ Lab9/Program.cs
@@ -153,18 +153,97 @@ using System.Collections.Generic;
 //    currentNode = currentNode.Previous;
 //}
 //Console.WriteLine();
+
 //6 вариант. Базовый уровень.
-OurLinkedList list =new OurLinkedList();
-Random random=new Random(); 
-for(int i=0; i < random.Next(10, 20); i++)
+//OurLinkedList list =new OurLinkedList();
+//Random random=new Random(); 
+//for(int i=0; i < random.Next(10, 20); i++)
+//{
+//    list.Add(random.Next(100));
+//}
+//list.Print();
+//double find=double.Parse(Console.ReadLine()!);
+//list.PrevRemove(find);
+//list.Print();
+
+//public class Node
+//{
+//    public Node(double data)
+//    {
+//        Data = data;
+//    }
+//    public double Data { get; set; }
+//    public Node Next { get; set; }
+//}
+
+//public class OurLinkedList  // односвязный список
+//{
+//    Node? head; // головной/первый элемент
+//    Node? tail; // последний/хвостовой элемент
+//    int count;  // количество элементов в списке
+
+//    // добавление элемента
+//    public void Add(double data)
+//    {
+//        Node node = new Node(data);
+
+//        if (head == null)
+//            head = node;
+//        else
+//            tail!.Next = node;
+//        tail = node;
+
+//        count++;
+//    }
+//    public void PrevRemove(double data)
+//    {
+//        if(head==null||head.Next==null) return;
+//        if(head.Data==data) return;
+//        if (head.Next.Data == data)
+//        {
+//            head=head.Next;
+//            return;
+//        }
+//        Node current=head;
+//        while(current.Next != null&&current.Next.Next!=null)
+//        {
+//            if (current.Next.Next.Data == data)
+//            {
+//                current.Next = current.Next.Next;
+//                return;
+//            }
+//            current = current.Next;
+//        }
+//    }
+//    public void Print()
+//    {
+//        if (head == null)
+//        {
+//            Console.WriteLine("Список пуст");
+//            return;
+//        }
+
+//        Node current = head;
+//        while (current != null)
+//        {
+//            Console.Write(current.Data + " ");
+//            current = current.Next;
+//        }
+//        Console.WriteLine();
+//    }
+//}
+
+//6 вариант. Средний уровень.
+OurLinkedList list = new OurLinkedList();
+Random random = new Random();
+for (int i = 0; i < random.Next(10, 20); i++)
 {
     list.Add(random.Next(100));
 }
 list.Print();
-double find=double.Parse(Console.ReadLine()!);
-list.PrevRemove(find);
+double find = double.Parse(Console.ReadLine()!);
+list.Dublicate(find);
 list.Print();
-
 public class Node
 {
     public Node(double data)
@@ -194,25 +273,22 @@ public class OurLinkedList  // односвязный список
 
         count++;
     }
-    public void PrevRemove(double data)
+    public bool Dublicate(double data)
     {
-        if(head==null||head.Next==null) return;
-        if(head.Data==data) return;
-        if (head.Next.Data == data)
+        Node current = head;
+        while (current!=null&&!current.Data.Equals(data))
         {
-            head=head.Next;
-            return;
+           current = current.Next;
         }
-        Node current=head;
-        while(current.Next != null&&current.Next.Next!=null)
+        if (current == null)
         {
-            if (current.Next.Next.Data == data)
-            {
-                current.Next = current.Next.Next;
-                return;
-            }
-            current = current.Next;
+           return false;
         }
+        Node copyNode= new Node(data);
+        copyNode.Next = current.Next;
+        current.Next = copyNode;
+        count++;
+        return true;
     }
     public void Print()
     {
@@ -231,4 +307,3 @@ public class OurLinkedList  // односвязный список
         Console.WriteLine();
     }
 }
-
