a305a26c4f8247024b77a2979de56a3304e66db0
rakdav <rakdav@inbox.ru>
2026-03-19 15:03:08 +0200

    sets

---
+++ Lab10/Program.cs
@@ -0,0 +1,75 @@
+﻿//Dictonary
+//Dictionary<int,string> people=new Dictionary<int,string>();
+//people.Add(4, "Bob");
+//people.Add(5, "Golub");
+//people.Add(1, "Nikitin");
+//foreach (var person in people)
+//    Console.WriteLine($"Key:{person.Key} value:{person.Value}");
+//foreach (var (key, val) in people) 
+//{
+//    Console.WriteLine($"key: {key}  value: {val}");
+//}
+//Console.WriteLine(people.Count);
+//Console.WriteLine(people[4]);
+//Console.WriteLine(people.ContainsKey(4));
+//Console.WriteLine(people.ContainsValue("Nikitin"));
+
+////Sets
+////HashSet
+//HashSet<string> fruits=new HashSet<string>();
+//fruits.Add("Potato");
+//fruits.Add("Banana");
+//fruits.Add("Apple");
+//fruits.Add("Orange");
+//fruits.Add("Banana");
+//foreach (var fru in fruits)
+//    Console.WriteLine(fru+" "+fru.GetHashCode());
+////SortedSet
+//SortedSet<string> treeFruit=new SortedSet<string>();
+//treeFruit.Add("Potato");
+//treeFruit.Add("Banana");
+//treeFruit.Add("Apple");
+//treeFruit.Add("Orange");
+//treeFruit.Add("Banana");
+//foreach (var fru in treeFruit) Console.WriteLine(fru);
+
+//SortedSet<int> intTree = new SortedSet<int>();
+//intTree.Add(34);
+//intTree.Add(12);
+//intTree.Add(25);
+//intTree.Add(78);
+//intTree.Add(64);
+//intTree.Add(99);
+//foreach (var t in intTree) Console.Write(t+" ");
+//Console.WriteLine();
+
+List<int> varinants = new List<int>();
+for (int i = 0; i < 30; i++) varinants.Add(i+1);
+Dictionary<string, int> people = new Dictionary<string, int>();
+HashSet<int> list = new HashSet<int>();
+int n = 1;
+while (n<11)
+{
+    int number = new Random().Next(1, 30);
+    if (!list.Contains(number))
+    {
+        list.Add(number);
+        n++;
+    }
+}
+foreach (int i in list) Console.Write(i+" ");
+Console.WriteLine();
+int j = 1;
+foreach (int i in list)
+{
+    people[(j).ToString()] = i;
+    j++;
+}
+foreach(var (key,value) in people)
+{
+    Console.WriteLine(key+":"+value);
+}
+
+
+
+
---
+++ OS2/Program.cs
@@ -0,0 +1,45 @@
+﻿//1
+Task task1 = new Task(()=> Console.WriteLine("Hello Task!"));
+task1.Start();
+//2
+Task task2 = Task.Factory.StartNew(() => Console.WriteLine("Hello Task!"));
+//3
+Task task3 = Task.Run(() => Console.WriteLine("Hello Task!"));
+
+task1.Wait();
+task2.Wait();
+task3.Wait();
+//асинхронно
+Console.WriteLine("Main Starts");
+Task task4 = new Task(() =>
+{
+    Console.WriteLine("Task Starts");
+    Thread.Sleep(1000);
+    Console.WriteLine("Task Ends");
+});
+Console.WriteLine("Main Ends");
+task4.Start();
+task4.Wait();
+//синхронно
+Console.WriteLine("Main Starts");
+Task task5 = new Task(() =>
+{
+    Console.WriteLine("Task Starts");
+    Thread.Sleep(1000);
+    Console.WriteLine("Task Ends");
+});
+Console.WriteLine("Main Ends");
+task5.RunSynchronously();
+//свойства Task
+Task task6 = new Task(() =>
+{
+    Console.WriteLine($"Task {Task.CurrentId} Starts");
+    Thread.Sleep(1000);
+    Console.WriteLine($"Task {Task.CurrentId} Ends");
+});
+task6.Start();
+Console.WriteLine(task6.Id);
+Console.WriteLine(task6.IsCompleted);
+Console.WriteLine(task6.IsCanceled);
+Console.WriteLine(task6.IsFaulted);
+Console.WriteLine(task6.Status);
