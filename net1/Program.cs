a3d13bbb766fedc12de72db479d2c86fee1b84c5
rakdav <rakdav@inbox.ru>
2026-03-23 19:05:12 +0200

    1

---
+++ net1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+
+IPAddress localIp = new IPAddress(new byte[] {127,0,0,1});
+Console.WriteLine(localIp);
+IPAddress someIp = new IPAddress(0x0100007F);
+Console.WriteLine(someIp);
+IPAddress ip=IPAddress.Parse("127.0.0.1");
+Console.WriteLine(ip);
+IPAddress anyIp = IPAddress.Any;
+Console.WriteLine(anyIp);
+IPAddress localhost = IPAddress.Loopback;
+Console.WriteLine(localhost);
+IPAddress broadcastIp=IPAddress.Broadcast;
+
+Console.WriteLine(localIp.AddressFamily);
+
+IPAddress myAdd = IPAddress.Parse("192.168.122.139");
+IPEndPoint endPoint = new IPEndPoint(myAdd, 8080);
+Console.WriteLine(endPoint.Address);
+Console.WriteLine(endPoint.Port);
+
+string uriString = "https://mail.ru/";
+Uri myURI= new Uri(uriString);
+Console.WriteLine(myURI.AbsoluteUri);
+Console.WriteLine(myURI.Host);
+Console.WriteLine(myURI.Port);
+Console.WriteLine(myURI.Authority);
+Console.WriteLine(myURI.IsAbsoluteUri);
+Console.WriteLine(myURI.IsFile);
+Console.WriteLine(myURI.Scheme);
+Console.WriteLine(myURI.IsLoopback);
+Console.WriteLine(myURI.Query);
+Console.WriteLine(myURI.Fragment);
+Console.WriteLine(myURI.UserInfo);
+Console.WriteLine(myURI.OriginalString);
+Console.WriteLine(myURI.IsDefaultPort);
+Console.WriteLine(myURI.PathAndQuery);
+
+Uri uri1 = new Uri("https://mail.ru/", UriKind.Absolute);
+Uri uri2 = new Uri("inbox", UriKind.Relative);
+Uri uri3 = new Uri("https://mail.ru/", UriKind.RelativeOrAbsolute);
+
+UriBuilder uriBuilder1 = new UriBuilder("https","metanit.com",443,"sharp/net");
+Uri uri4 = uriBuilder1.Uri;
+Console.WriteLine(uri4);
+
+
+UriBuilder uriBuilder2 = new UriBuilder();
+uriBuilder2.Scheme = "https";
+uriBuilder2.Host = "metanit.com";
+uriBuilder2.Port = 80;
+Uri uri5= uriBuilder2.Uri;
+Console.WriteLine(uri5);
