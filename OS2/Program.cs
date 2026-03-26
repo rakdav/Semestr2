223f14b26e2c1881a2d5852e4b13fa21e36dca4e
rakdav <rakdav@inbox.ru>
2026-03-26 13:15:54 +0200

    ghfhgf

--- OS2/Program.cs
+++ OS2/Program.cs
@@ -92,26 +92,59 @@
 //    Console.WriteLine($"Id предыдущей задачи:{t.Id}");
 //    Thread.Sleep(3000);
 //}
-Console.Write("Введите кординату x1 точки:");
-double x1 = double.Parse(Console.ReadLine()!);
-Console.Write("Введите кординату y1 точки:");
-double y1 = double.Parse(Console.ReadLine()!);
-Console.Write("Введите кординату x2 точки:");
-double x2 = double.Parse(Console.ReadLine()!);
-Console.Write("Введите кординату y2 точки:");
-double y2 = double.Parse(Console.ReadLine()!);
-Console.Write("Введите кординату x3 точки:");
-double x3 = double.Parse(Console.ReadLine()!);
-Console.Write("Введите кординату y3 точки:");
-double y3 = double.Parse(Console.ReadLine()!);
-Task<double> task1 =Task.Run(()=>Side(x1,y1,x2,y2));
-Task<double> task2 = Task.Run(() => Side(x2, y2, x3, y3));
-Task<double> task3 = Task.Run(() => Side(x1, y1, x3, y3));
-Task<double> task4 = Task.Run(() =>Area(task1.Result,task2.Result,task3.Result));
-Console.WriteLine($"{task4.Result:F2}");
-double Side(double x1, double y1, double x2, double y2)=>Math.Sqrt(x1 * x1 + y1 * y2);
-double Area(double a,double b,double c)
+//Console.Write("Введите кординату x1 точки:");
+//double x1 = double.Parse(Console.ReadLine()!);
+//Console.Write("Введите кординату y1 точки:");
+//double y1 = double.Parse(Console.ReadLine()!);
+//Console.Write("Введите кординату x2 точки:");
+//double x2 = double.Parse(Console.ReadLine()!);
+//Console.Write("Введите кординату y2 точки:");
+//double y2 = double.Parse(Console.ReadLine()!);
+//Console.Write("Введите кординату x3 точки:");
+//double x3 = double.Parse(Console.ReadLine()!);
+//Console.Write("Введите кординату y3 точки:");
+//double y3 = double.Parse(Console.ReadLine()!);
+//Task<double> task1 =Task.Run(()=>Side(x1,y1,x2,y2));
+//Task<double> task2 = Task.Run(() => Side(x2, y2, x3, y3));
+//Task<double> task3 = Task.Run(() => Side(x1, y1, x3, y3));
+//Task<double> task4 = Task.Run(() =>Area(task1.Result,task2.Result,task3.Result));
+//Console.WriteLine($"{task4.Result:F2}");
+//double Side(double x1, double y1, double x2, double y2)=>Math.Sqrt(x1 * x1 + y1 * y2);
+//double Area(double a,double b,double c)
+//{
+//    double pp = (a + b + c) / 2;
+//    return Math.Sqrt(pp*(pp-a)*(pp-b)*(pp-c));
+//}
+
+//Класс Parallel
+//Parallel.Invoke(
+//    Print,
+//    () =>
+//    {
+//        Console.WriteLine($"Выполняется задача {Task.CurrentId}");
+//        Thread.Sleep(3000);
+//    },
+//    ()=>Square(5)
+//    );
+
+//Parallel.For
+//Parallel.For(1, 5, Square);
+//Parallel.ForEach
+ParallelLoopResult result = Parallel.ForEach<int>(
+    new List<int>() { 1, 3, 5, 7 },
+    Square);
+if(!result.IsCompleted)
+    Console.WriteLine($"Выполнение цикла завершено на итерации {result.LowestBreakIteration}");
+void Print()
 {
-    double pp = (a + b + c) / 2;
-    return Math.Sqrt(pp*(pp-a)*(pp-b)*(pp-c));
+    Console.WriteLine($"Выполняется задача {Task.CurrentId}");
+    Thread.Sleep(3000);
+}
+
+void Square(int n,ParallelLoopState pls)
+{
+    if(n==5) pls.Break();
+    Console.WriteLine($"Выполняется задача {Task.CurrentId}");
+    Thread.Sleep(3000);
+    Console.WriteLine($"Результат {n * n}");
 }
