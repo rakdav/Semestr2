4cd17e4a6d7a2985a30518e6cdb71ae0f07aff92
rakdav <rakdav@inbox.ru>
2026-04-25 14:11:31 +0200

    mvvmlab1

---
+++ OSLab1/View/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using OSLab1.ViewModel;
+using System.Drawing;
+using System.IO;
+using System.Linq.Expressions;
+using System.Windows;
+
+
+namespace OSLab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel(this);
+        }
+    }
+}
---
+++ OSLab1/ViewModel/MainViewModel.cs
@@ -0,0 +1,84 @@
+﻿using OSLab1.Command;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace OSLab1.ViewModel
+{
+    public class MainViewModel
+    {
+        private CancellationTokenSource _cancelToken = new CancellationTokenSource();
+        public MainWindow? MainWindow {  get; set; }
+        public MainViewModel(MainWindow _mainWindow) 
+        {
+            MainWindow = _mainWindow;
+        }
+        #region Commands
+        private RelayCommand processCommand;
+        public RelayCommand ProcessCommand
+        {
+            get
+            {
+                return processCommand ?? (processCommand = new RelayCommand(obj =>
+                {
+                    Task.Factory.StartNew(() => ProcessFiles());
+                }));
+            }
+        }
+        private RelayCommand cancelCommand;
+        public RelayCommand CancelCommand
+        {
+            get
+            {
+                return cancelCommand ?? (cancelCommand = new RelayCommand(obj =>
+                {
+                    _cancelToken.Cancel();
+                }));
+            }
+        }
+        #endregion
+        #region Fuctions
+        private void ProcessFiles()
+        {
+            ParallelOptions parOpts = new ParallelOptions();
+            parOpts.CancellationToken = _cancelToken.Token;
+            parOpts.MaxDegreeOfParallelism = Environment.ProcessorCount;
+            var bathPath = Directory.GetCurrentDirectory();
+            var pictureDirectory = Path.Combine(bathPath, "TestPictures");
+            var outputDirectory = Path.Combine(bathPath, "ModifiedPictures");
+            if (Directory.Exists(outputDirectory)) Directory.Delete(outputDirectory, true);
+            Directory.CreateDirectory(outputDirectory);
+            string[] files = Directory.GetFiles(pictureDirectory, "*.jpg", SearchOption.AllDirectories);
+            //foreach (string file in files)
+            try
+            {
+                Parallel.ForEach(files, file =>
+                {
+                    parOpts.CancellationToken.ThrowIfCancellationRequested();
+                    string filename = Path.GetFileName(file);
+                    //this.Title = $"Процесс {filename} в потоке {Thread.CurrentThread.ManagedThreadId}";
+                    MainWindow!.Dispatcher?.Invoke(() =>
+                    {
+                        MainWindow.Title = $"Процесс {file}";
+                    });
+                    using (Bitmap bitmap = new Bitmap(file))
+                    {
+                        bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        bitmap.Save(Path.Combine(outputDirectory, filename));
+                    }
+                });
+                MainWindow!.Dispatcher?.Invoke(() => { MainWindow.Title = "Завершено!"; });
+            }
+            catch (OperationCanceledException ex)
+            {
+                MainWindow!.Dispatcher?.Invoke(() => { MainWindow.Title = ex.Message; });
+            }
+        }
+        #endregion
+    }
+}
