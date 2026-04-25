b192650f1dd081faad149efaaa53f2b718509d67
rakdav <rakdav@inbox.ru>
2026-04-25 13:23:47 +0200

    Lab1OS

--- OSLab1/MainWindow.xaml.cs
+++ OSLab1/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
+﻿using System.Drawing;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace OSLab1
 {
@@ -16,9 +10,57 @@ namespace OSLab1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private CancellationTokenSource _cancelToken = new CancellationTokenSource();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void cmdCancel_Click(object sender, RoutedEventArgs e)
+        {
+            _cancelToken.Cancel();
+        }
+
+        private void cmdProcess_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Factory.StartNew(()=> ProcessFiles());
+            this.Title = "Processing Complete";
+        }
+        private void ProcessFiles()
+        {
+            ParallelOptions parOpts= new ParallelOptions();
+            parOpts.CancellationToken=_cancelToken.Token;
+            parOpts.MaxDegreeOfParallelism=Environment.ProcessorCount;
+            var bathPath = Directory.GetCurrentDirectory();
+            var pictureDirectory = Path.Combine(bathPath, "TestPictures");
+            var outputDirectory = Path.Combine(bathPath, "ModifiedPictures");
+            if(Directory.Exists(outputDirectory)) Directory.Delete(outputDirectory, true);
+            Directory.CreateDirectory(outputDirectory);
+            string[] files = Directory.GetFiles(pictureDirectory, "*.jpg", SearchOption.AllDirectories);
+            //foreach (string file in files)
+            try
+            {
+                Parallel.ForEach(files, file =>
+                {
+                    parOpts.CancellationToken.ThrowIfCancellationRequested();
+                    string filename = Path.GetFileName(file);
+                    //this.Title = $"Процесс {filename} в потоке {Thread.CurrentThread.ManagedThreadId}";
+                    Dispatcher?.Invoke(() =>
+                    {
+                        this.Title = $"Процесс {file}";
+                    });
+                    using (Bitmap bitmap = new Bitmap(file))
+                    {
+                        bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        bitmap.Save(Path.Combine(outputDirectory, filename));
+                    }
+                });
+                Dispatcher?.Invoke(() => { this.Title = "Завершено!"; });
+            }
+            catch (OperationCanceledException ex)
+            {
+                Dispatcher?.Invoke(() => { this.Title = ex.Message; });
+            }
         }
     }
 }
