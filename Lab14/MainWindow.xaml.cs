894971b13cc6c26bb4c5bdba7d70fdda97954edf
rakdav <rakdav@inbox.ru>
2026-05-04 15:21:57 +0200

    lab14

---
+++ Lab14/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Lab14
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double x1 = 100;
+        private double y1 = 100;
+        private double x2 = 500;
+        private double y2 = 100;
+        private double x3 = 500;
+        private double y3 = 500;
+        private double x4 = 100;
+        private double y4 = 500;
+        private DispatcherTimer timer;
+        private int side = 1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer=new DispatcherTimer();
+            Draw(x1, y1, x2, y2, x3, y3, x4, y4);
+        }
+        private void Draw(double _x1,double _y1,
+                          double _x2, double _y2,
+                          double _x3, double _y3,
+                          double _x4, double _y4)
+        {
+            canva.Children.Clear();
+            Polygon polygon = new Polygon();
+            polygon.Points = new PointCollection();
+            polygon.Points.Add(new Point(_x1, _y1));
+            polygon.Points.Add(new Point(_x2, _y2));
+            polygon.Points.Add(new Point(_x3, _y3));
+            polygon.Points.Add(new Point(_x4, _y4));
+            polygon.Fill = Brushes.Red;
+            canva.Children.Add(polygon);
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            if (side == 1)
+            {
+                x4 = x1 += 10;
+                x2 = x3 -= 10;
+            }
+            else if (side == 2) 
+            {
+                x4 = x1 -= 10;
+                x2 = x3 += 10;
+            }
+            if(x1==x2) side=2;
+            if(x1==100&&x2==500) side=3;
+            if(y1==y4) side=4;
+            if (y1 == 100 && y4 == 500) side = 1;
+            Draw(x1, y1, x2, y2, x3, y3, x4, y4);
+        }
+    }
+}
